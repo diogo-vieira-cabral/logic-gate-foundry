6f45541756e007e59278b2ef12d4738e539db015
diogo-vieira-cabral <diogo.vieira.cabral@gmail.com>
2026-01-29 14:30:11 +0000

    docs: 185 notes update

--- sql/leetcode/185.sql
+++ sql/leetcode/185.sql
@@ -1,9 +1,12 @@
--- 185. Department Top Three Salaries  
+/*
+-- 185. Department Top Three Salaries
 
--- BUSINESS CONTEXT: This logic allows management to identify "Top-Tier Clusters" rather than just outliers.  
+-- BUSINESS CONTEXT: This logic allows management to identify "Top-Tier Clusters" rather than just outliers.
 
--- TECHNICAL NOTES: Using DENSE_RANK() over RANK() for Data integrity. If multiple employees share a top salary, they still occupy one "rank". 
-
+-- TECHNICAL NOTES: 
+	Using DENSE-RANK over RANK - If multiple employees share a top salary, they still occupy one "rank".
+	Joining inside the CTE - ensuring that the ranking logic is applied to a fully-formed business entity (Employee + Dept) rather than a raw ID, avoiding 'bloat' in the final layer.
+*/
 
 WITH HighEarners AS (
 SELECT
@@ -19,18 +22,17 @@ FROM
 JOIN
     Department AS d ON e.departmentId = d.id 
 ) 
--- Joining inside the CTE promotes Data Hygiene. It ensures that the ranking logic is applied to a fully-formed business entity (Employee + Dept) rather than a raw ID, avoiding 'bloat' in the final layer.
 
 SELECT
-Department, -- defined "universe" at pre calculation (CTE) allows 
+Department, 
 Employee,
 Salary
 FROM
-    HighEarners -- Temp table name used for calculation
+    HighEarners
 WHERE
     rnk <= 3; -- change int to change salary cluster
 
 /* BUSINESS RECOMMENDATION: 
-If a department shows more than 5 employees in the 'Top 3' rnk by department, it indicates a 'Heavy Top' salary regime. 
-Recommend a review of the 'Junior-to-Senior' ratio to ensure long-term budget sustainability and prevent wage compression. 
+Departments where the 'Top 3' salaries are identical suggest a highly commoditized role structure.
+Recommend performance-based bonus restructuring to incentivize individual 'Alpha' contributions.
 */
